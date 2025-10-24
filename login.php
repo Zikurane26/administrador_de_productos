a1d98ebd3703e0d11950cdfddbfbd14a58b73b77
Fabian-58 <134320971+Fabian-58@users.noreply.github.com>
2025-10-24 17:05:20 -0500

    Change stylesheet link to estilo_login.css

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
   <!-- Estilos -->
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="estilo_login.css">
 </head>
 <body>
   <form class="login-container" method="POST" action="validar_login.php">
