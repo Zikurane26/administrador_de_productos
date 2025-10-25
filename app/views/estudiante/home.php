98a9fa508bce23086348017da06a38e7726e1d08
Zikurane <Brasti00@gmail.com>
2025-10-25 16:28:05 -0500

    Validación de estudiante

--- app/views/estudiante/home.php
+++ app/views/estudiante/home.php
@@ -9,24 +9,6 @@
   <a href="logout.php">Cerrar sesión</a>
   <hr>
 
-  <?php
-  require 'vendor/autoload.php';
-  use MongoDB\Client;
-
-  $uri = getenv('MONGODB_URI') ?: 'mongodb+srv://Zikurane26:TKzk2LN9YsJxA9x2@cluster0.umwdkel.mongodb.net/?retryWrites=true&w=majority&tls=true';
-  $client = new Client($uri);
-  $database = $client->selectDatabase('test_db');
-  $collection = $database->selectCollection('usuarios');
-
-  $cursor = $collection->find();
-
-  echo "<h3>Datos BD</h3><ul>";
-  foreach ($cursor as $doc) {
-      echo "<li>" . htmlspecialchars(json_encode($doc)) . "</li>";
-  }
-  echo "</ul>";
-  ?>
-  
   <h1>👋 Bienvenido Estudiante, <?= htmlspecialchars($usuario['nombre']); var_dump($_SESSION['usuario']);?></h1>
   <a href="/public/index.php?controller=Auth&action=logout">Cerrar sesión</a>
   <hr>
