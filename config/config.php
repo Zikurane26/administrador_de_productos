3ebfe95efcde005d7c8ed2ed045c85e9f133c576
Zikurane <Brasti00@gmail.com>
2025-10-25 14:46:53 -0500

    Validación de BD

---
+++ config/config.php
@@ -0,0 +1,7 @@
+HOLA CAPICHO
+
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+define('MONGO_URI', 'mongodb+srv://Zikurane26:TKzk2LN9YsJxA9x2@cluster0.umwdkel.mongodb.net/?retryWrites=true&w=majority&tls=true');
+define('MONGO_DB', 'asistencia_app');
