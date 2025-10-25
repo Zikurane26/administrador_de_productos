64925fe70e4b3c33f7ae05cca8761eb29b716e38
Zikurane <Brasti00@gmail.com>
2025-10-25 17:44:55 -0500

    Redirección a la BD correcta

--- app/views/profesor/home.php
+++ app/views/profesor/home.php
@@ -5,40 +5,24 @@
   <title>Panel del Profesor</title>
 </head>
 <body>
-  <h1>Bienvenido Profesor, <?php echo htmlspecialchars($_SESSION['usuario']); var_dump($_SESSION['usuario']); ?> 👋</h1>
+  <h1>👨‍🏫 Bienvenido, <?= htmlspecialchars($_SESSION['nombre']) ?></h1>
   <a href="/public/index.php?controller=Auth&action=logout">Cerrar sesión</a>
   <hr>
 
-  <h1>Lista de Usuarios</h1>
-
-  <table border="1" cellpadding="8">
-      <thead>
+  <h2>Usuarios registrados</h2>
+  <table border="1" cellpadding="5">
+      <tr>
+          <th>ID</th>
+          <th>Usuario</th>
+          <th>Rol</th>
+      </tr>
+      <?php foreach ($usuarios as $user): ?>
           <tr>
-              <th>ID</th>
-              <th>Nombre</th>
-              <th>Usuario</th>
-              <th>Rol</th>
+              <td><?= htmlspecialchars($user['_id']) ?></td>
+              <td><?= htmlspecialchars($user['usuario']) ?></td>
+              <td><?= htmlspecialchars($user['rol']) ?></td>
           </tr>
-      </thead>
-      <tbody>
-          <?php foreach ($usuarios as $usuario): ?>
-              <tr>
-                  <td><?= htmlspecialchars($usuario['id']) ?></td>
-                  <td><?= htmlspecialchars($usuario['nombre']) ?></td>
-                  <td><?= htmlspecialchars($usuario['usuario']) ?></td>
-                  <td><?= htmlspecialchars($usuario['rol']) ?></td>
-              </tr>
-          <?php endforeach; ?>
-      </tbody>
+      <?php endforeach; ?>
   </table>
-
-  
-  <h2>Crear lista de asistencia</h2>
-  <form action="/public/index.php?controller=profesor&action=crearLista" method="POST">
-      <button type="submit">Generar código de asistencia</button>
-  </form>
-
-  <hr>
-  <a href="/public/index.php?controller=profesor&action=verListas">Ver listas creadas</a>
 </body>
 </html>
